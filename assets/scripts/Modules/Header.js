dd9bcdedcb92d09c7bfc28f306a65e3be2024c6e
webNekit <mochalov-hunter@mail.ru>
2025-01-08 01:03:14 +0300

    create modal form

--- assets/scripts/Modules/Header.js
+++ assets/scripts/Modules/Header.js
@@ -1,6 +1,6 @@
 class Header {
     selectors = {
-        root: '[data-header]',
+        root: '[data-header]', // Главный элемент шапки
         overlay: '[data-overlay-menu]',
         toglerButton: '[data-burger-menu]',
     }
@@ -8,16 +8,27 @@ class Header {
     stateClasses = {
         isActive: 'is-active',
         isLock: 'is-lock',
+        isScroll: 'is-scroll', // Класс для подчеркивания
     }
 
     bindEvents() {
-        this.toglerButtonElement.addEventListener("click", this.onToglerButtonClick)
+        this.toglerButtonElement.addEventListener("click", this.onToglerButtonClick);
+        window.addEventListener("scroll", this.onScroll); // Слушаем событие прокрутки
     }
 
     onToglerButtonClick = () => {
         this.toglerButtonElement.classList.toggle(this.stateClasses.isActive);
         this.overlayElement.classList.toggle(this.stateClasses.isActive);
         document.documentElement.classList.toggle(this.stateClasses.isLock);
+    }
+
+    onScroll = () => {
+        const scrollThreshold = 50; // Порог скролла в пикселях
+        if (window.scrollY > scrollThreshold) {
+            this.rootElement.classList.add(this.stateClasses.isScroll);
+        } else {
+            this.rootElement.classList.remove(this.stateClasses.isScroll);
+        }
     }
 
     constructor() {
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,11 @@
 import Header from "./Modules/Header.js";
-
 new Header();
+
+import Modal from "./Modules/Modal.js";
+new Modal();
+
+const element = document.getElementById('phone');
+const maskOptions = {
+    mask: '+{7}(000)000-00-00'
+};
+const mask = IMask(element, maskOptions);
